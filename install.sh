149589faf7b6f7b7a5fb6f1311480141c0fad24a
AstralCam <AstralCam@protonmail.com>
2021-03-12 19:41:09 -0600

    Agregue mis snippets de latex y markdown

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -r -s home/.bg.png ~/.bg.png
 ln -r -s home/.vimrc ~/.vimrc
 ln -r -s home/.gvimrc ~/.gvimrc
+ln -r -s home/.vim/UltiSnips/ ~/.vim/UltiSnips
 ln -r -s home/.spectrwm.conf ~/.spectrwm.conf
 ln -r -s home/.autostart.sh ~/.autostart.sh
 ln -r -s home/.Xresources ~/.Xresources
